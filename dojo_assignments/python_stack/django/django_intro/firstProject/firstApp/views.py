ccb8b79dc965f3b0f85453e0d6253c14c66480b4
Justin Smith <mistahj@gmail.com>
2020-05-06 14:12:20 -0700

    getting all new changes and files included

---
+++ dojo_assignments/python_stack/django/django_intro/firstProject/firstApp/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render, HttpResponse
+def index(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
--- dojo_assignments/python_stack/my_environments/djangoPy3Env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mistahj/Desktop/coding_dojo/dojo_assignments/python_stack/my_environments/djangoPy3Env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
